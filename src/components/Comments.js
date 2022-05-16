df6937d541995c2dfa40fc5798893b143baeec40
Mark Tailor <markvtailor@gmail.com>
2022-05-16 20:49:08 +0300

    first commit

---
+++ src/components/Comments.js
@@ -0,0 +1,27 @@
+import { useState,useEffect } from "react"
+import axios from "axios";
+const BASE_URL='http://localhost:8080'
+
+
+const Comments = (props) =>{
+
+    const [comments,setComments] = useState([]);
+    const fetchComments = () => {
+      axios.get(BASE_URL+"/stats/video/comments/" + props.id).then(res => {
+        setComments(res.data);
+      })
+    };
+    useEffect(() => {
+      fetchComments();
+    },[]);
+
+    return comments.map((commentEntity,index) => {
+        return (<div key={index}>
+          <h1>{commentEntity.author}</h1>
+          <text>{commentEntity.body}</text>
+          <p>{commentEntity.date}</p>
+        </div>)
+      })
+}
+
+export default Comments;
---
+++ src/hooks/CommentVideo.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+const BASE_URL='http://localhost:8080'
+
+const CommentVideo = (props) =>{
+
+    const postComment = async () =>{
+        const response = await axios.post(BASE_URL+"/stats/video/comment/",JSON.stringify({author: props.author,body: props.body,videoId: props.id}),
+        {
+            headers: {'Content-Type': 'application/JSON'},
+        });
+
+        return response.data;
+    }
+
+    return(
+            <button onClick={()=>postComment()}>Отправить</button>
+    )
+}
+
+export default CommentVideo;
